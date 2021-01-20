1fa90c22ed733f10d227b7f457a3bf83f37cc678
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-01-20 06:53:47 +0500

    added partition,coinchange

---
+++ algorithms/lib/algorithms_degi.dart
@@ -0,0 +1,219 @@
+import 'dart:math';
+
+// Iterative function to find longest increasing subsequence
+// of given array
+void findLIS(List<int> arr, int n) {
+// LIS[i] stores the longest increasing subsequence of subarray
+// arr[0..i] that ends with arr[i]
+  //var LIS =new List<List<int>>(n);
+  var LIS = List.generate(n, (i) => List<int>(), growable: false);
+
+// LIS[0] denotes longest increasing subsequence ending with arr[0]
+  LIS[0].add(arr[0]);
+
+// start from second element in the array
+  for (int i = 1; i < n; i++) {
+// do for each element in subarray arr[0..i-1]
+    for (int j = 0; j < i; j++) {
+// find longest increasing subsequence that ends with arr[j]
+// where arr[j] is less than the current element arr[i]
+
+      if (arr[j] < arr[i] && LIS[j].length > LIS[i].length) {
+        LIS[i] = List.from(LIS[j]);
+      }
+    }
+
+// include arr[i] in LIS[i]
+    LIS[i].add(arr[i]);
+  }
+
+// uncomment below lines to print contents of vector LIS
+/* for (int i = 0; i < n; i++)
+    {
+        cout << "LIS[" << i << "] - ";
+        for (int j : LIS[i])
+            cout << j << " ";
+        cout << endl;
+    } */
+
+// j will contain index of LIS
+
+  int j=0;
+  for (int i = 0; i < n; i++) {
+    if (LIS[j].length < LIS[i].length) {
+      j = i;
+    }
+  }
+
+// print LIS
+// for (int i : LIS[j]) {
+//   print("$i ");
+// }
+  print(LIS[j]);
+}
+
+String callLIS(List<int> arr) {
+  int n = arr.length;
+  findLIS(arr, n);
+  return "LIS";
+}
+
+
+
+//MCM
+
+// Function to find the most efficient way to multiply
+// given sequence of matrices
+int MatrixChainMultiplication(List<int> dims, int n) {
+// c[i,j] = Minimum number of scalar multiplications (i.e., cost)
+// needed to compute the matrix M[i]M[i+1]...M[j] = M[i..j]
+// The cost is zero when multiplying one matrix
+  var c = List.generate(n + 1, (i) => List<int>(n + 1), growable: false);
+
+  for (int i = 1; i <= n; i++) {
+    c[i][i] = 0;
+  }
+
+  for (int len = 2; len <= n; len++) // Subsequence lengths
+      {
+    for (int i = 1; i <= n - len + 1; i++) {
+      int j = i + len - 1;
+      c[i][j] = 9223372036854775807; //maxInt64
+
+      for (int k = i; j < n && k <= j - 1; k++) {
+        int cost = c[i][k] + c[k + 1][j] + dims[i - 1] * dims[k] * dims[j];
+
+        if (cost < c[i][j]) {
+          c[i][j] = cost;
+        }
+      }
+    }
+  }
+  return c[1][n - 1];
+}
+
+// Matrix Chain Multiplication Problem
+String callMCM(List<int> dims) {
+// Matrix M[i] has dimension dims[i-1] x dims[i] for i = 1..n
+// input is 10 x 30 matrix, 30 x 5 matrix, 5 x 60 matrix
+  int n = dims.length;
+  print("Minimum cost is ${MatrixChainMultiplication(dims, n)}");
+  return "Minimum cost is ${MatrixChainMultiplication(dims, n)}";
+}
+
+
+//Partition Problem
+// Return true if there exists a subset of array[0..n) with given sum
+bool subsetSum(List<int> arr, int n, int sum) {
+// T[i][j] stores true if subset with sum j can be attained with
+// using items up to first i items
+  var T = List.generate(n + 1, (i) => List<bool>(sum + 1), growable: false);
+
+//bool T[n + 1][sum + 1];
+
+// if 0 items in the list and sum is non-zero
+  for (int j = 1; j <= sum; j++) {
+    T[0][j] = false;
+  }
+
+// if sum is zero
+  for (int i = 0; i <= n; i++) {
+    T[i][0] = true;
+  }
+
+// do for ith item
+  for (int i = 1; i <= n; i++) {
+// consider all sum from 1 to sum
+    for (int j = 1; j <= sum; j++) {
+// don't include ith element if j-arr[i-1] is negative
+      if (arr[i - 1] > j) {
+        T[i][j] = T[i - 1][j];
+      } else
+// find subset with sum j by excluding or including the ith item
+        T[i][j] = T[i - 1][j] || T[i - 1][j - arr[i - 1]];
+    }
+  }
+
+// return maximum value
+  return T[n][sum];
+}
+
+// Partition problem - Return true if given array arr[0..n-1] can
+// be divided into two subsets with equal sum
+bool partition(List<int> arr, int n) {
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+
+// return true if sum is even and array can can be divided into
+// two subsets with equal sum
+  return (sum%2==0) && subsetSum(arr, n, sum ~/ 2);
+}
+
+// main function to solve partition problem
+String callPartP(List<int> arr) {
+// number of items
+  int n = arr.length;
+
+  if (partition(arr, n)) {
+    print("Yes");
+    return "Yes";
+  } else {
+    print("No");
+    return "No";
+  }
+}
+
+
+
+
+
+//Coin Change Problem
+const INT_MAX = 9223372036854775807;
+// Function to find the minimum number of coins required
+// to get total of N from set S
+int findMinCoins(List<int> S, int n, int N) {
+// T[i] stores minimum number of coins needed to get total of i
+  List<int> T = new List<int>(N + 1);
+//int T[N + 1];
+  T[0] = 0; // 0 coins are needed to get total of i
+
+  for (int i = 1; i <= N; i++) {
+// initialize minimum number of coins needed to infinity
+    T[i] = INT_MAX;
+    int res = INT_MAX;
+
+// do for each coin
+    for (int c = 0; c < n; c++) {
+// check if index doesn't become negative by including
+// current coin c
+      if (i - S[c] >= 0) {
+        res = T[i - S[c]];
+      }
+
+// if total can be reached by including current coin c,
+// update minimum number of coins needed T[i]
+      if (res != INT_MAX) {
+        T[i] = min(T[i], res + 1);
+      }
+    }
+  }
+
+// T[N] stores the minimum number of coins needed to get total of N
+  return T[N];
+}
+
+String callCoinChangeP(List<int> S,int N) {
+// n coins of given denominations
+  int n = S.length;
+
+// Total Change required int N
+
+  int coins = findMinCoins(S, n, N);
+
+  if (coins != INT_MAX) {
+    print("Minimum number of coins required to get desired change is $coins");
+    return "Minimum number of coins required to get desired change is $coins";
+  }
+}
--- algorithms/lib/input.dart
+++ algorithms/lib/input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:algorithms/algorithms_abc.dart';
-import 'package:algorithms/algorithms_de.dart';
+import 'package:algorithms/algorithms_degi.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 
@@ -19,14 +19,14 @@ class _AlgoMenuState extends State<AlgoMenu> {
   void runAlgo(int input) async{
     var path = "assets/inputs/";
     LineSplitter ls = new LineSplitter();
-    Future<String> data;
     print(widget.opt);
+    //abc
     if(widget.opt=="LCS" || widget.opt=="SCS" || widget.opt=="LevenD"){
       path += "abc/${input}.csv";
-      data = loadAsset(path);
-      String X,Y;
-      var result = await data;
+      var result = await loadAsset(path);
       print("File Contents: $result");
+
+      String X,Y;
       List<String> result2= ls.convert(result);
       print(result2);
       X=result2[0];
@@ -41,21 +41,29 @@ class _AlgoMenuState extends State<AlgoMenu> {
         callLevenD(X,Y);
       }
     }
-    else if(widget.opt=="LIS" || widget.opt=="MCM"){
-      path += "de/${input}.csv";
+    //degi
+    else if(widget.opt=="LIS" || widget.opt=="MCM" || widget.opt=="PartP" || widget.opt=="CoinChangeP"){
+      path += "degi/${input}.csv";
       var result = await loadAsset(path);
       print("File Contents: $result");
-      var result2=json.decode(result).cast<int>();;
+
+      var result2=json.decode("[" +result + "]").cast<int>();
       print(result2);
-      //List<int> result3 = result2.map((data) => int.parse(data)).toList();
-      print("Result3 is: $result2");
+      print("Result2 is: $result2");
       if(widget.opt=="LIS"){
         callLIS(result2);
       }
       else if(widget.opt=="MCM"){
         callMCM(result2);
       }
+      else if(widget.opt=="PartP"){
+        callPartP(result2);
+      }
+      else if(widget.opt=="CoinChangeP"){
+        callCoinChangeP(result2,0169);
+      }
     }
+
     //setState(){};
   }
 
